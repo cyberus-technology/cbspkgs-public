895c7442b10dca44b635f1492d2ea7e79b357a9f
Jacek Galowicz <jacek.galowicz@cyberus-technology.de>
2020-04-02 13:19:16 +0200

    Add cyberus CA and binary cache to cbspkgs-public

--- modules/default.nix
+++ modules/default.nix
@@ -1,4 +1,9 @@
 { niv }:
 {
   hydra = import "${niv.hydra}/hydra-module.nix";
+
+  cyberus = {
+    certificateAuthority = import ./cyberus-ca.nix;
+    binaryCache = import ./cyberus-binary-cache.nix;
+  };
 }
