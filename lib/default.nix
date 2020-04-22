06ba1326486154d451abc8badf1b66b6bd5bb14e
Martin Messer <martin.messer@cyberus-technology.de>
2020-04-22 14:14:51 +0200

    makeInitrdQemuTest: make qemu tests transformation public available

--- lib/default.nix
+++ lib/default.nix
@@ -5,6 +5,7 @@
   sotest = import ./sotest.nix { inherit pkgs; };
   writers = import ./writers.nix { inherit pkgs; };
   makeInitrd = import ./patched-make-initrd.nix { inherit pkgs; };
+  makeInitrdQemuTest = import ./qemu-test/qemu-test.nix { inherit pkgs; };
 
   path = ./.;
 }
--- pkgs/initrd-creator/release.nix
+++ pkgs/initrd-creator/release.nix
@@ -4,10 +4,9 @@ with pkgs.lib;
 
 let
   initrds = import ./image_defs { inherit pkgs cbsLib; };
-  initrdTest = import ./lib/qemu-test.nix pkgs;
   transformToTest = initrdName: initrdDrv: nameValuePair
     ("${initrdName}-test")
-    (initrdTest { initrd = "${initrdDrv}/initrd"; inherit (initrdDrv) timeoutSeconds; });
+    (cbsLib.makeInitrdQemuTest { initrd = "${initrdDrv}/initrd"; inherit (initrdDrv) timeoutSeconds; });
 in {
   inherit initrds;
   tests = mapAttrs' transformToTest initrds;
