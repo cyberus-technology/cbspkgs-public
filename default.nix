081d6c3ac55503a65ac28cbf07be75b998378f95
Jacek Galowicz <jacek.galowicz@cyberus-technology.de>
2020-07-08 13:42:15 +0200

    parametrize nixpkgs

--- default.nix
+++ default.nix
@@ -1,9 +1,10 @@
 { sources ? import ./nix/sources.nix // { cbspkgs-public = ./.; }
+, nixpkgs ? sources.nixpkgs
 , ovl ? import ./nix/cyberus-overlay-function.nix {
     inherit sources;
     overlayRepoList = [ "cbspkgs-public" ];
   }
-, pkgs ? import sources.nixpkgs { inherit (ovl) overlays; }
+, pkgs ? import nixpkgs { inherit (ovl) overlays; }
 }:
 
 {
