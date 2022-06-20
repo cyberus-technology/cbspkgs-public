a71e3dd08085d577c3793977093874ced0088255
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2022-06-20 17:20:57 +0200

    lib: remove cartesian set utils
    nixpkgs has lib.cartesianProductOfSets for a while now, so there is no
    reason to carry the same functionality in cbspkgs.

--- lib/default.nix
+++ lib/default.nix
@@ -1,7 +1,6 @@
 { pkgs }:
 
 {
-  cartesian = import ./cartesian.nix { inherit pkgs; };
   sotest = import ./sotest.nix { inherit pkgs; };
   writers = import ./writers.nix { inherit pkgs; };
   makeInitrd = import ./patched-make-initrd.nix { inherit pkgs; };
